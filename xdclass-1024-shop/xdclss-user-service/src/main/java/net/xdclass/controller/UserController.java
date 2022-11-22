68dd3893d84c69abdcbab83f6673934094b0cdfd
agang <agang@qq.com>
2022-11-22 11:46:07 +0800

    创建

---
+++ xdclass-1024-shop/xdclss-user-service/src/main/java/net/xdclass/controller/UserController.java
@@ -0,0 +1,21 @@
+package net.xdclass.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 阿刚
+ * @since 2022-11-22
+ */
+@RestController
+@RequestMapping("/userDO")
+public class UserController {
+
+}
+
