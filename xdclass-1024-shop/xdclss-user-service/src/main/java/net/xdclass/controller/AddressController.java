0368974a07ae461e56e285b2549d5964ca9fd6f9
agang <agang@qq.com>
2022-11-23 11:27:40 +0800

    统一枚举类和响应协议

--- xdclass-1024-shop/xdclss-user-service/src/main/java/net/xdclass/controller/AddressController.java
+++ xdclass-1024-shop/xdclss-user-service/src/main/java/net/xdclass/controller/AddressController.java
@@ -4,7 +4,9 @@ package net.xdclass.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import net.xdclass.model.AddressDO;
 import net.xdclass.service.AddressService;
+import net.xdclass.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +35,8 @@ public class AddressController {
     public Object detai(
             @ApiParam(value = "地址id", required = true)
             @PathVariable("address_id") Long addressId) {
-        return addressService.detai(addressId);
+        AddressDO addressDO = addressService.detai(addressId);
+        return JsonData.buildSuccess(addressDO);
     }
 }
 
